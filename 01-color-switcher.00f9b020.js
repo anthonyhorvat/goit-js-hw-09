4145bdbf33105373831fe709e7e725f92103d226
anthonyhorvat <111517271+anthonyhorvat@users.noreply.github.com>
2023-09-24 13:43:08 +0000

    Deploying to gh-pages from @ anthonyhorvat/goit-js-hw-09@b3fec6be8e3f015306d913976ba8325138139153 🚀

---
+++ 01-color-switcher.00f9b020.js
@@ -0,0 +1,2 @@
+!function(){document.getElementsByTagName("body");var t,e=document.querySelector("[data-start]"),n=document.querySelector("[data-stop]");e.addEventListener("click",(function(){e.disabled=!0,t=setInterval((function(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}),1e3)})),n.addEventListener("click",(function(){e.disabled=!1,clearInterval(t)}))}();
+//# sourceMappingURL=01-color-switcher.00f9b020.js.map
--- 01-color-switcher.c2d7ac32.js
+++
@@ -1,2 +0,0 @@
-!function(){document.getElementsByTagName("body");var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]");t.addEventListener("click",(function(){t.disabled=!0,timerId=setInterval((function(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}),1e3)})),e.addEventListener("click",(function(){t.disabled=!1,clearInterval(timerId)}))}();
-//# sourceMappingURL=01-color-switcher.c2d7ac32.js.map
